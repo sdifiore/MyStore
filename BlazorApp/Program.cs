ea6d74e83449cdb953908835b9e16e1fe65bc470
Sergio Di Fiore <difiores@outlook.com>
2025-08-08 15:39:19 -0300

    Authentication set-up

--- BlazorApp/Program.cs
+++ BlazorApp/Program.cs
@@ -1,5 +1,7 @@
 using BlazorApp;
+using BlazorApp.Services;
 
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -8,5 +10,9 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddCascadingAuthenticationState();
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
 
 await builder.Build().RunAsync();
---
+++ BlazorApp/Services/CustomAuthStateProvider.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+
+using System.Security.Claims;
+
+namespace BlazorApp.Services
+{
+	public class CustomAuthStateProvider : AuthenticationStateProvider
+	{
+		public override Task<AuthenticationState> GetAuthenticationStateAsync()
+		{
+			var user = new ClaimsPrincipal(new ClaimsIdentity()); // No user is authenticated by default
+
+			return Task.FromResult(new AuthenticationState(user));
+		}
+		public void NotifyAuthenticationStateChanged(AuthenticationState state)
+		{
+			NotifyAuthenticationStateChanged(Task.FromResult(state));
+		}
+	}
+}
